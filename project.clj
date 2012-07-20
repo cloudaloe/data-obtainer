a5fd86e6e7632bef9dac64c085fb75e2cafc3a66
matan <matan@cloudaloe.org>
2012-07-21 00:42:13 +0300

    no comment

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject data-obtainer "0.1.0-SNAPSHOT"
   :description "an application for learning Clojure while doing someting useful"
-  :dependencies [[org.clojure/clojure "1.3.0"] [org.clojure/java.jdbc "0.2.3"] [mysql/mysql-connector-java "5.1.18"]]
+  :dependencies [[org.clojure/clojure "1.3.0"] [org.clojure/java.jdbc "0.2.3"]]
+  :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.21"]]}}
   :main data-obtainer.core)
