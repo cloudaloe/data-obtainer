b7e5ea7e99f4379a5e1039cd845d82cb4c7ef73f
matan <matan@cloudaloe.org>
2012-07-29 14:15:51 +0300

    Open-up config file for 'JDBC properties'
    Any trailing attributes will be passed to the JDBC driver as properties.
    
    (As per 'DriverManager' section in http://clojure.github.com/java.jdbc/)

--- src/data_obtainer/obtain.clj
+++ src/data_obtainer/obtain.clj
@@ -9,30 +9,31 @@
 			 {:mysql-column "index_length" :human-name "Index Size"}])
 
 (defn obtain [configuration]
-	(let [{:keys [host port schema user password]} configuration] 
-		(let [subname (str "//" host ":" port "/information_schema")] 
-			(let [mysql-conn {:subprotocol "mysql" :subname subname :user user :password password}] 
-				(let [time (java.sql.Timestamp. (.getTime (java.util.Date.)))] ; a(. System (nanoTime)) based timestamping implementation or no-system call service can be considered
-					(try
-						(sql/with-connection mysql-conn
-							(sql/with-query-results rows
-								["select table_name,
-								table_rows,
-								data_length,
-								index_length
-								from information_schema.TABLES
-								where table_schema=?" schema] 
-								(if rows 
-									(do (doseq [row rows] (println (str "Obtained metadata for:" " host " host " schema " schema " table " (:table_name row) ".")))
-										(doseq [row rows] (writeline (str 
-											time "," host "," schema ","
-											(:table_name row) "," 
-											(:table_rows row) "," 
-											(:data_length row) ","
-											(:index_length row)
-									\newline)))) 
-									(println (str \newline "--- Warning:" \newline "In MySQL, on host " host ", no metadata obtained for schema " schema \newline "------------" \newline))
-								)
-							)
-						)
-						(catch Exception e (println (str \newline "--- Warning:" \newline "Could not query one MySQL database for table sizes." \newline "Attempted connection map was: " \newline mysql-conn "." \newline "Source configuration was:" \newline configuration \newline "Exception encountered:" \newline e \newline "------------" \newline)))))))))	
+	(let [{:keys [host port schema]} configuration]
+	(let [rest-of-configuration (dissoc configuration :host :port :schema)]
+	(let [subname (str "//" host ":" port "/information_schema")] 
+	(let [mysql-conn (conj {:subprotocol "mysql" :subname subname :schema schema}, rest-of-configuration)] 
+	(let [time (java.sql.Timestamp. (.getTime (java.util.Date.)))] ; a(. System (nanoTime)) based timestamping implementation or no-system call service can be considered
+		(try
+			(sql/with-connection mysql-conn
+				(sql/with-query-results rows
+					["select table_name,
+					table_rows,
+					data_length,
+					index_length
+					from information_schema.TABLES
+					where table_schema=?" schema] 
+					(if rows 
+						(do (doseq [row rows] (println (str "Obtained metadata for:" " host " host " schema " schema " table " (:table_name row) ".")))
+							(doseq [row rows] (writeline (str 
+								time "," host "," schema ","
+								(:table_name row) "," 
+								(:table_rows row) "," 
+								(:data_length row) ","
+								(:index_length row)
+						\newline)))) 
+						(println (str \newline "--- Warning:" \newline "In MySQL, on host " host ", no metadata obtained for schema " schema \newline "------------" \newline))
+					)
+				)
+			)
+			(catch Exception e (println (str \newline "--- Warning:" \newline "Could not query one MySQL database for table sizes." \newline "Attempted connection map was: " \newline mysql-conn "." \newline "Source configuration was:" \newline configuration \newline "Exception encountered:" \newline e \newline "------------" \newline))))))))))	
